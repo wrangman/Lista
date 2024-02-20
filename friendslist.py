373bc2da82da7ea2d15b78504c09da32c6acecfc
wrangman <wrangman@gmail.com>
2024-02-20 09:50:06 +0100

    dsaf

--- friendslist.py
+++ friendslist.py
@@ -6,21 +6,32 @@ __version__ = "1.0.0"
 __email__   = "johan.wrango@ntig.se"
 '''
 
-
 import os
 
 
 def show_friends():
     os.system('cls')
     print("-------- MINA VÄNNER --------")
-    for i in range(len(friends_list)):
-        print(f"{i+1}) {friends_list[i]}")
-    print(f"\n Antal vänner: {str(len(friends_list))}\n")
+    for i, name in enumerate(friends_list):
+        print(f"{i+1}) {name}")
+        
+    print(f"\nAntal vänner: {len(friends_list)}\n")
 
 
 def rename(f_index, f_name):
     if friends_list[f_index] != f_name and f_name != "":
         friends_list[f_index] = f_name
+
+
+def custom_text(friends_list):
+    if len(friends_list) > 1:
+        return f"Lägg till/Ta bort/Ändra (1-{str(len(friends_list))}): "
+        
+    elif len(friends_list) > 0:
+        return "Lägg till/Ta bort/Ändra: "
+
+    elif len(friends_list) > -1:
+        return "Lägg till en ny vän: "
 
 
 # Här börjar programmet
@@ -34,20 +45,13 @@ Lägg till vän - skriv nytt namn
 Ta bort vän - skriv samma namn
 Ändra namn - välj nummer i lista
 ---------------------------------
-               Avsluta med /quit.
+                   Avsluta med /q
 ---------------------------------""")
 
 while True:
-    if len(friends_list) > 1:
-        modify = input(f"Lägg till/Ta bort/Ändra (1-{str(len(friends_list))}): ")
-        
-    elif len(friends_list) > 0:
-        modify = input("Lägg till/Ta bort/Ändra: ")
+    modify = input(custom_text(friends_list))
 
-    elif len(friends_list) > -1:
-        modify = input("Lägg till en ny vän: ")
-
-    if modify.upper() == "/QUIT":                # Avsluta
+    if modify.upper() == "/QUIT" or modify.upper() == "/Q":                # Avsluta
         break
     if not modify.isdigit():
         try:
